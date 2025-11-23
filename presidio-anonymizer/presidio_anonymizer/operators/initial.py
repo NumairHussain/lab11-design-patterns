48b8388a38c076f0c831aa49478db89b49e1081d
Numair Hussain <123851476+NumairHussain@users.noreply.github.com>
2025-11-23 20:20:10 +0000

    Updated inital class and made the test pass

--- presidio-anonymizer/presidio_anonymizer/operators/initial.py
+++ presidio-anonymizer/presidio_anonymizer/operators/initial.py
@@ -5,8 +5,16 @@ from presidio_anonymizer.operators import Operator
 
 class Initial(Operator):
     def operate(self, text: str, params: Dict = None) -> str:
-        return ""
-    
+        split_text = text.split()
+
+        initials = ""
+        for text in split_text:
+            if text:
+                initials += text[0].upper() + ". "
+
+        initials = initials.strip()
+        return initials
+
     def validate(self, params: Dict = None) -> None:
         pass
 
